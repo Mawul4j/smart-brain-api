44f8d7f765d314a61d2a60959045a6f94ab75227
Mawulolo Ameko <mawulolo.ameko@gmail.com>
2023-07-10 14:02:54 -0400

    basic smart-api functionality

---
+++ server.js
@@ -0,0 +1,91 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "mawulolo",
+      email: "mawulolo@gmail.com",
+      password: "orange",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "orlane",
+      email: "orlane@gmail.com",
+      password: "mango",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
+// --> res = this is working
+app.get("/", (req, res) => {
+  res.json(database.users);
+});
+
+//signin --> POST = sucess/fail
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success!");
+  } else {
+    res.status(400).json("error logging in!");
+  }
+});
+
+//register --> POST = user
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+//profile/:userId --> GET = user
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(400).json("no such user, try a new id!");
+  }
+});
+
+//image --> PUT --> user
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+  if (!found) {
+    res.status(400).json("no such user, try a new id!");
+  }
+});
+//listen on the portal
+app.listen(3000, () => {
+  console.log("don't worry your app is good!");
+});
