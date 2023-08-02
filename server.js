5b84f4595a5ad7b222c191f6cd246bdf6d26673e
Mawulolo Ameko <mawulolo.ameko@gmail.com>
2023-08-01 21:56:06 -0400

    add db functionality to image and profile functions

--- server.js
+++ server.js
@@ -60,10 +60,6 @@ app.post("/signin", (req, res) => {
 //register --> POST = user
 app.post("/register", (req, res) => {
   const { name, email, password } = req.body;
-  // bcrypt.hash(password, null, null, function (err, hash) {
-  //   // Store hash in your password DB.
-  //   console.log(hash);
-  // });
   db("users")
     .returning("*")
     .insert({
@@ -80,31 +76,30 @@ app.post("/register", (req, res) => {
 app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
   let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  });
-  if (!found) {
-    res.status(400).json("no such user, try a new id!");
-  }
+  db.select("*")
+    .from("users")
+    .where({ id: id })
+    .then((user) => {
+      // console.log(user);
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("user not found");
+      }
+    })
+    .catch((err) => res.status(400).json("error getting user"));
 });
-
 //image --> PUT --> user
 app.put("/image", (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach((user) => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
-  });
-  if (!found) {
-    res.status(400).json("no such user, try a new id!");
-  }
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0].entries);
+    })
+    .catch((err) => res.status(400).json("unable to update entries"));
 });
 
 // bcrypt.hash("bacon", null, null, function (err, hash) {
